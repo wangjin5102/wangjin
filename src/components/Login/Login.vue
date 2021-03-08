5dffa1d0e9d6302d17d1c44fecdffbca77068d90
jin.wang <wangjin5102@163.com>
2021-03-08 10:05:42 +0800

    登录

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,35 +1,104 @@
 <template>
-  <div>
-    <v-btn elevation="2">11</v-btn>
-    <v-text-field>1</v-text-field>
+  <div :style="`background-image:url(${bg}); height: calc(100vh); background-size: cover`">
+    <validation-observer
+      ref="observer"
+      v-slot="{ invalid }"
+      class='form'
+    >
+      <form @submit.prevent="submit">
+        <validation-provider
+          v-slot="{ errors }"
+          name="Name"
+          rules="required|max:10"
+        >
+          <v-text-field
+            v-model="name"
+            :counter="10"
+            :error-messages="errors"
+            label="Name"
+            required
+          ></v-text-field>
+        </validation-provider>
+
+        <validation-provider
+          v-slot="{ errors }"
+          rules="required"
+          name="checkbox"
+        >
+          <v-checkbox
+            v-model="checkbox"
+            :error-messages="errors"
+            value="1"
+            label="自动登录"
+            type="checkbox"
+            required
+          ></v-checkbox>
+        </validation-provider>
+
+        <v-btn
+          class="mr-4"
+          type="submit"
+          :disabled="invalid"
+        >
+          登录
+        </v-btn>
+        <v-btn @click="clear">
+          重置
+        </v-btn>
+      </form>
+    </validation-observer>
   </div>
 </template>
 
 <script>
+  import { required, max } from 'vee-validate/dist/rules'
+  import { extend, ValidationObserver, ValidationProvider, setInteractionMode } from 'vee-validate'
+
+  setInteractionMode('eager')
+
+  extend('required', {
+    ...required,
+    message: '{_field_} can not be empty',
+  })
+
+  extend('max', {
+    ...max,
+    message: '{_field_} may not be greater than {length} characters',
+  })
 export default {
   name: 'HelloWorld',
+  components: {
+    ValidationProvider,
+    ValidationObserver,
+  },
+
+
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      name: '',
+      msg: 'Welcome to Your Vue.js App',
+      bg: '../../../static/images/bg.png'
     }
-  }
+  },
+  methods: {
+      submit () {
+        this.$refs.observer.validate()
+      },
+      clear () {
+        this.name = ''
+        this.phoneNumber = ''
+        this.email = ''
+        this.select = null
+        this.checkbox = null
+        this.$refs.observer.reset()
+      },
+    },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .form form {
+    background: #fff;
+  }
 </style>
